f3b8e4feafcb4d6d231df2e11894a95b2e1db9a3
Morgan Jones <morgan@morganjones.org>
2017-05-19 13:33:09 -0400

    basic functionality, able to add an attr (maybe more) to a csv list of users

--- csv2ldap.pl
+++ csv2ldap.pl
@@ -2,68 +2,86 @@
 #
 # morgan@morganjones.org
 # import a csv file into ldap--used to bulk add users from a csv.
-# Not the most straightforward to use and needs to be generalized but got the job done.
-
-# $ perl -pi -e 's/\r/\n/g' Morgan_Charter_Import.csv
-# $ ~/Docs/git/ldap_utils/import_from_csv.pl < ~morgan/Desktop/Morgan_Charter_Import.csv |ldapmodify -a -x -H ldaps://devldapm01.domain.org -y pass
-
-# to delete if applicable:
-# $ ldapsearch -LLL -x  -y ~/Docs/.pass -H ldaps://devldapm01.domain.org orgaccountstatuslog="*for Compass*" dn| sed 's/dn: //' | ldapdelete  -v  -D uid=morgan,ou=employees,dc=domain,dc=org -y ~/Docs/.pass -H ldaps://devldapm01.domain.org
-#
-# for i in `./import_from_csv.pl ~/Desktop/Morgan_Charter_Import.csv 2>&1 | grep skipping|sed 's/skipping //'|sed 's/\.\.\.//'`; do echo $i ; echo; done|ldapdelete -H ldaps://devldapm01.domain.net -D uid=morgan,ou=employees,dc=domain,dc=org -v -y ~/Docs/.pass 
-
+# only has basic functionality, needs to be expanded
 
 use strict;
+use Net::LDAP;
+use Getopt::Std;
+use Data::Dumper;
 
-my @fields = qw/orgeidn uid userpassword cn givenname sn orgmiddlename orgsuffix orghomeorgcd orghomeorg orgsponsorhomeorgcd orgsponsorhomeorg orgsponsoreidn orgvendorname mail orgworktelephonedid orgassociateusertypecd orgassociateusertype orgstateid sambasid/;
 
-my @common_fields = qw/objectClass inetorgperson objectClass ntUser objectClass orgAssociate objectClass top objectClass sambaSamAccount objectClass organizationalPerson objectClass person objectClass inetuser orgaccountstatus active orgaccountactive TRUE/;
+my %opts;
+getopts('nf:c:b:y:H:D:s', \%opts);
 
-#push @common_fields, ("orgAccountStatusLog", "201508170000000Z; 0000060473-kacless imported user for Compass from Oracle External Users");
+print "-n used, no changes will be made\n"
+  if (exists $opts{n});
 
-my $i=0;
-while (<>) {
-    next if ($i++ < 1);
+exists $opts{H} || printUsage();
+exists $opts{b} || printUsage();
 
-    my @field_values = split /,/;
+my $ldap = Net::LDAP->new($opts{H}) or die "$@";
 
-    next if ($field_values[0] eq "orgEIDN");
+my $pfh;
+open ($pfh, $opts{y});
+my $pass = <$pfh>;
+close $pass;
 
-    my $empid = $field_values[0];
-    next unless ($empid);
+$ldap->bind($opts{D}, password=>$pass);
 
-    my $entry = `ldapsearch -D uid=morgan,ou=employees,dc=domain,dc=org -x -w 'pass' -H ldaps://ldapm01.domain.net -LLLb dc=domain,dc=org orgeidn=$empid dn objectclass`;
+my @fields = split (/,/, $opts{f});
+
+my $indexattr = shift @fields;
+
+my $csvfh;
+
+open ($csvfh, $opts{c}) || die "can't open $opts{c}";
+
+# ignore the header if so requested
+<$csvfh> if (exists $opts{s});
+
+while (<$csvfh>) {
+    my @values = split /,/;
+    my $indexvalue = shift @values;
+    my $filter = "(" . ${indexattr} . "=" . ${indexvalue} . ")";
+    print "\n$filter\n";
     
-    my $dn = $1
-        if ($entry =~ /dn:\s*([^\n]+)\n/);
+    my $rslt = $ldap->search(base=>$opts{b}, filter=>$filter);
+    $rslt->code && warn "problem searching: " . $rslt->error;
 
-    if (defined $dn) {
-	print STDERR "skipping $dn...\n";
+    my $entry = $rslt->as_struct();
+
+#    print Dumper $entry;
+
+    my @DNs = keys %$entry;
+    if ($#DNs!=0) {
+	warn "$filter didn't return exactly one entry, ignoring";
 	next;
     }
 
-    my $dn2add = "uid=" . $field_values[1] . ",ou=employees,dc=domain,dc=org";
+    my $dn = $DNs[0];
+    
+    my $i=0;
+    while (my $v = shift @values) {
+	my $a = $fields[$i++];
 
-    print "dn: $dn2add\n";
-
-    my $h = 0;
-    for (@common_fields) {
-	if (!($h % 2)) {
-	    print $common_fields[$h];
-	} else {
-	    print ": ", $common_fields[$h], "\n";
+	if (exists $entry->{$dn}{lc $a}) {
+	    print "Skipping $dn, it already contains $a: @{$entry->{$dn}{lc $a}}\n";
+	    next;
 	}
-	$h++;
-    }
-      
 
-    my $i = 0;
-    for (@fields) {
-	chomp $field_values[$i];
-	print $fields[$i] , ": " , $field_values[$i] . "\n";
-	$i++;
+	chomp $v;
+	print "adding $a $v to $dn\n";
+
+	if (!exists $opts{n}) {
+	    my $mod_rslt = $ldap->modify($dn, add => { $a => $v });
+	    $mod_rslt->code && die "problem modifying: " . $mod_rslt->error;
+	}
+				  
     }
-    print "ntuserdomainid: $field_values[1]\n";
-    print "orgaccountstatuslog: 20160220000000Z 9000000015-morgan created from ldif for kacless\n";
-    print "\n";
+}
+
+sub printUsage {
+    print "usage: $0 [-s] -H ldapurl -D binddn -y passfile -f indexfield,field1,field2,... -c csv\n";
+    print "\t-s skip header in csv file\n";
+    exit 0;
 }
